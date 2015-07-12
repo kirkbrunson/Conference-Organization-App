1e8024b7aa0cdad5c6f7ad030d4fa8eb6605c8eb
Kirk Brunson <kirkbrunson314@gmail.com>
2015-07-12 12:50:53 -0400

    Changed client id and app name

--- settings.py
+++ settings.py
@@ -12,7 +12,7 @@ created/forked from conference.py by wesc on 2014 may 24
 
 # Replace the following lines with client IDs obtained from the APIs
 # Console or Cloud Console.
-WEB_CLIENT_ID = 'replace with Web client ID'
+WEB_CLIENT_ID = '567065799550-dgh6k5u715rp7fg9m5biirk9q6e639vn.apps.googleusercontent.comD'
 ANDROID_CLIENT_ID = 'replace with Android client ID'
 IOS_CLIENT_ID = 'replace with iOS client ID'
 ANDROID_AUDIENCE = WEB_CLIENT_ID
